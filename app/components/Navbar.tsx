40035e41e808d5ee09d9077202bc0bd621b04582
AdrianRMV <adrianthelawx@gmail.com>
2023-09-05 23:02:20 -0700

    Pages has been added

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import Menu from './Menu';
+
+const Navbar = () => {
+    return (
+        <div>
+            {/* LOGO */}
+            <div>
+                <Link href="/">Massimo</Link>
+            </div>
+            {/* MOBILE MENU */}
+            <div>
+                <Menu />
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
