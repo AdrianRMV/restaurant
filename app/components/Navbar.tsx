50a875fa582ec11c4bec0126e1aa0f59747618db
AdrianRMV <adrianthelawx@gmail.com>
2023-09-07 18:23:25 -0700

    Header changes, with responsive design with it

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,16 +1,42 @@
 import Link from 'next/link';
 import Menu from './Menu';
+import CartIcon from './CartIcon';
+import Image from 'next/image';
+
+const user = false;
 
 const Navbar = () => {
     return (
-        <div>
-            {/* LOGO */}
-            <div>
-                <Link href="/">Massimo</Link>
+        <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24 lg:px-20 xl:px-40">
+            {/* LEFT LINKS */}
+            <div className="hidden md:flex gap-4 flex-1">
+                <Link href="/">Homepage</Link>
+                <Link href="/menu">Menu</Link>
+                <Link href="/">Contact Us</Link>
             </div>
+
+            {/* LOGO */}
+            <div className="text-xl md:font-bold flex-1 md:text-center">
+                <Link href="/">The Bear</Link>
+            </div>
+
             {/* MOBILE MENU */}
-            <div>
+            <div className="md:hidden">
                 <Menu />
+            </div>
+
+            {/* RIGHT LINKS */}
+            <div className="hidden md:flex gap-4 items-center flex-1 justify-end">
+                <div className="md:absolute top-2 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 p-1 rounded-md">
+                    <Image src="/phone.png" alt="" width={20} height={20} />
+                    <span>612 3482 563</span>
+                </div>
+                {!user ? (
+                    <Link href="/">Login</Link>
+                ) : (
+                    <Link href="/menu">Orders</Link>
+                )}
+                <CartIcon />
             </div>
         </div>
     );
