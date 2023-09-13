6dadd73f7af495742ae11f84f51f3d51a452d13e
AdrianRMV <adrianthelawx@gmail.com>
2023-09-12 18:39:12 -0700

    More changes

--- app/components/Featured.tsx
+++ app/components/Featured.tsx
@@ -1,68 +1,41 @@
 'use client';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
 
-import { useEffect, useRef } from 'react';
-import Image from 'next/image';
 import { featuredProducts } from '../data';
 
+import Image from 'next/image';
+
 const Featured = () => {
-    const sliderRef = useRef<HTMLDivElement>(null);
-
-    // For the slider (Later)
-    useEffect(() => {
-        const slider = sliderRef.current;
-        let isDown = false;
-        let startX: number;
-        let scrollLeft: number;
-
-        const handleMouseDown = (e: MouseEvent) => {
-            isDown = true;
-            startX = e.pageX;
-            scrollLeft = slider!.scrollLeft;
-        };
-
-        const handleMouseLeave = () => {
-            isDown = false;
-        };
-
-        const handleMouseUp = () => {
-            isDown = false;
-        };
-
-        const handleMouseMove = (e: MouseEvent) => {
-            if (!isDown) return;
-            e.preventDefault();
-            const x = e.pageX;
-            const walk = x - startX;
-            slider!.scrollLeft -= walk;
-            startX = e.pageX; // Actualizamos startX para el siguiente movimiento
-        };
-
-        if (slider) {
-            slider.addEventListener('mousedown', handleMouseDown);
-            slider.addEventListener('mouseleave', handleMouseLeave);
-            slider.addEventListener('mouseup', handleMouseUp);
-            slider.addEventListener('mousemove', handleMouseMove);
-        }
-
-        return () => {
-            if (slider) {
-                slider.removeEventListener('mousedown', handleMouseDown);
-                slider.removeEventListener('mouseleave', handleMouseLeave);
-                slider.removeEventListener('mouseup', handleMouseUp);
-                slider.removeEventListener('mousemove', handleMouseMove);
-            }
-        };
-    }, []);
-
     return (
-        <div className="w-screen overflow-x-hidden text-red-500">
+        <Swiper
+            pagination={{
+                dynamicBullets: true,
+            }}
+            modules={[Pagination]}
+            slidesPerView={1}
+            breakpoints={{
+                //  >= 768px (md)
+                768: {
+                    slidesPerView: 2,
+                },
+                //  >= 1024px (lg)
+                1024: {
+                    slidesPerView: 3,
+                },
+            }}
+            className="w-screen overflow-x-scroll text-red-500"
+        >
             {/* WRAPPER */}
-            <div ref={sliderRef} className="inline-flex space-x-4 cursor-grab">
+            <div className="w-max flex">
                 {/* SINGLE ITEM */}
                 {featuredProducts.map((item) => (
-                    <div
+                    <SwiperSlide
                         key={item.id}
-                        className="w-screen h-[60vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-50 transition-all duration-300 md:w-[50vw] xl:w-[33vw] xl:h-[90vh]"
+                        className="!w-screen !h-[60vh] !flex !flex-col !items-center !justify-around !p-4 hover:bg-fuchsia-50 transition-all duration-300 md:!w-[50vw] xl:!w-[33vw] xl:!h-[90vh] cursor-grab"
                     >
                         {/* IMAGE CONTAINER */}
                         {item.img && (
@@ -71,27 +44,27 @@ const Featured = () => {
                                     src={item.img}
                                     alt=""
                                     fill
-                                    className="object-contain"
+                                    className="object-contain relative"
                                 />
                             </div>
                         )}
                         {/* TEXT CONTAINER */}
-                        <div className="flex-1 flex flex-col gap-4 items-center justify-center text-center">
-                            <h1 className="text-lg font-bold uppercase xl:text-2xl 2xl:text-3xl">
+                        <div className=" flex-1 flex flex-col items-center justify-center text-center gap-4">
+                            <h1 className="text-xl font-bold uppercase xl:text-2xl 2xl:text-3xl">
                                 {item.title}
                             </h1>
                             <p className="p-4 2xl:p-8">{item.desc}</p>
                             <span className="text-xl font-bold">
                                 ${item.price}
                             </span>
-                            <button className="bg-red-500 text-white p-2 rounded-md transition duration-300 hover:bg-red-600">
+                            <button className="bg-red-500 text-white p-2 rounded-md">
                                 Add to Cart
                             </button>
                         </div>
-                    </div>
+                    </SwiperSlide>
                 ))}
             </div>
-        </div>
+        </Swiper>
     );
 };
 
--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,6 +1,16 @@
+import Image from 'next/image';
 
 const SingleProduct = () => {
-    return <div>SingleProduct</div>;
+    return (
+        <div className="p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-red-500 md:flex-row">
+            {/* IMAGE CONTAINER  */}
+            <div className="">
+                <Image src="" alt="" fill className="object-contain" />
+            </div>
+            {/* TEXT CONTAINER */}
+            <div className=""></div>
+        </div>
+    );
 };
 
 export default SingleProduct;
