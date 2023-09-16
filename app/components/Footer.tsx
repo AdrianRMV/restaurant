071b7db3d66d5c84b226212c7d4e33b81d1dbc1e
AdrianRMV <adrianthelawx@gmail.com>
2023-09-15 18:12:21 -0700

    Few more changes on styles

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const Footer = () => {
     return (
-        <div className="h-12 md:h-16 p-4 lg:p-10 xl:p-20 text-red-500 flex items-center justify-between">
+        <div className="h-12 md:h-16 p-4 lg:p-10 xl:p-20 text-red-500 flex items-center justify-between ">
             <Link href={'/'} className="font-bold text-xl">
                 THE BEAR
             </Link>
--- app/components/Price.tsx
+++ app/components/Price.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { useState } from 'react';
+
 type Props = {
     price: number;
     id: number;
@@ -8,15 +12,28 @@ type Props = {
 };
 
 const Price = ({ price, id, options }: Props) => {
+    const [total, setTotal] = useState(price);
+    const [quantity, setQuantity] = useState(1);
+    const [selected, setSelected] = useState(0);
+
     return (
         <div className=" flex flex-col gap-4">
-            <h2 className="text-2xl font-bold">${price.toFixed(2)}</h2>
+            <h2 className="text-2xl font-bold">${total.toFixed(2)}</h2>
             {/* OPTIONS CONTAINER */}
             <div className="flex gap-4">
-                {options?.map((option) => (
+                {options?.map((option, index) => (
                     <button
                         key={option.title}
-                        className="p-2 ring-1 ring-red-400 rounded-md"
+                        className="min-w-[6rem] p-2 ring-1 ring-red-400 rounded-md"
+                        style={{
+                            background:
+                                selected === index
+                                    ? 'rgb(248 113 113)'
+                                    : 'white',
+                            color:
+                                selected == index ? 'white' : 'rgb(239 68 68)',
+                        }}
+                        onClick={() => setSelected(index)}
                     >
                         {option.title}
                     </button>
@@ -25,16 +42,18 @@ const Price = ({ price, id, options }: Props) => {
             {/* QUANTITY AND ADD BOTTON CONTAINER */}
             <div className="flex justify-between items-center">
                 {/* QUANTITY */}
-                <div className="">
+                <div className="flex justify-between w-full p-3 ring-1 ring-red-500">
                     <span>Quantity</span>
-                    <div>
+                    <div className="flex gap-4 items-center">
                         <button>{'<'}</button>
-                        <span>1</span>
+                        <span>{quantity}</span>
                         <button>{'>'}</button>
                     </div>
                 </div>
                 {/* CART BUTTON */}
-                <button className="uppercase bg-red-500">Add to Cart</button>
+                <button className="uppercase w-56 bg-red-500 text-white p-3 ring-1 ring-red-400">
+                    Add to Cart
+                </button>
             </div>
         </div>
     );
