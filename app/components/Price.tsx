593b7af102f2dd49fb3a9ca54cb633e9850f653c
AdrianRMV <adrianthelawx@gmail.com>
2023-09-15 23:47:34 -0700

    Almost there

--- app/components/Price.tsx
+++ app/components/Price.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 type Props = {
     price: number;
@@ -15,6 +15,13 @@ const Price = ({ price, id, options }: Props) => {
     const [total, setTotal] = useState(price);
     const [quantity, setQuantity] = useState(1);
     const [selected, setSelected] = useState(0);
+
+    useEffect(() => {
+        setTotal(
+            quantity *
+                (options ? price + options[selected].additionalPrice : price)
+        );
+    }, [quantity, selected, options, price]);
 
     return (
         <div className=" flex flex-col gap-4">
@@ -45,9 +52,19 @@ const Price = ({ price, id, options }: Props) => {
                 <div className="flex justify-between w-full p-3 ring-1 ring-red-500">
                     <span>Quantity</span>
                     <div className="flex gap-4 items-center">
-                        <button>{'<'}</button>
+                        <button
+                            onClick={() =>
+                                setQuantity(
+                                    quantity === 1 ? quantity : quantity - 1
+                                )
+                            }
+                        >
+                            {'<'}
+                        </button>
                         <span>{quantity}</span>
-                        <button>{'>'}</button>
+                        <button onClick={() => setQuantity((prev) => prev + 1)}>
+                            {'>'}
+                        </button>
                     </div>
                 </div>
                 {/* CART BUTTON */}
