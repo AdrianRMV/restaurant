bc780fd0021adf722a90cdd891555d0da7576a36
AdrianRMV <adrianthelawx@gmail.com>
2023-09-16 18:37:23 -0700

    Styles has been done

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,11 +2,17 @@ import Link from 'next/link';
 
 const Footer = () => {
     return (
-        <div className="h-12 md:h-16 p-4 lg:p-10 xl:p-20 text-red-500 flex items-center justify-between ">
+        <div className="h-12 md:h-16 p-4 lg:px-10 xl:px-20 text-red-500 flex items-center justify-between ">
             <Link href={'/'} className="font-bold text-xl">
                 THE BEAR
             </Link>
-            <p>ALL RIGHTS RESERVED.</p>
+            <Link
+                href="https://github.com/AdrianRMV"
+                className="text-sm uppercase font-bold text-center"
+            >
+                Adrian Ramirez
+            </Link>
+            <p className="text-sm md:text-base">ALL RIGHTS RESERVED.</p>
         </div>
     );
 };
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,58 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 const LoginPage = () => {
-    return <div>LoginPage</div>;
+    return (
+        <div className="h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] p-4 flex items-center justify-center">
+            {/* BOX */}
+            <div className="h-full shadow-2xl rounded-md flex flex-col md:flex-row md:h-[70%] md:w-full lg:w-[60%] 2xl:w-1/2">
+                {/* IMAGE CONTAINER */}
+                <div className="relative h-1/3 w-full md:h-full md:w-1/2">
+                    <Image
+                        src="/loginBg.png"
+                        alt=""
+                        fill
+                        className="object-cover"
+                    />
+                </div>
+                {/* FORM CONTAINER */}
+                <div className="p-10 flex flex-col gap-8 md:h-full md:w-1/2">
+                    <h1 className="font-bold text-2xl xl:text-3xl">Welcome</h1>
+                    <p className="">
+                        Log into your account or create a new one using social
+                        buttons
+                    </p>
+                    {/* SOCIAL LOGIN BUTTONS */}
+                    <button className="flex gap-4 p-4 ring-1 ring-orange-100 rounded-md">
+                        <Image
+                            src="/google.png"
+                            alt=""
+                            width={20}
+                            height={20}
+                            className="object-contain"
+                        />
+                        <span>Sing in with Google</span>
+                    </button>
+                    <button className="flex gap-4 p-4 ring-1 ring-blue-100 rounded-md">
+                        <Image
+                            src="/facebook.png"
+                            alt=""
+                            width={20}
+                            height={20}
+                            className="object-contain"
+                        />
+                        <span>Sing in with Faceebook</span>
+                    </button>
+                    <p className="text-sm">
+                        Have a problem?{' '}
+                        <Link href="/contact-us" className="underline">
+                            Contact us
+                        </Link>
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default LoginPage;
