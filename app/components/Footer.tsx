f88edbfe93f1a335cd23fe42e2b11cdc09c74b00
AdrianRMV <adrianthelawx@gmail.com>
2023-09-16 18:44:00 -0700

    Im the best

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,17 +2,25 @@ import Link from 'next/link';
 
 const Footer = () => {
     return (
-        <div className="h-12 md:h-16 p-4 lg:px-10 xl:px-20 text-red-500 flex items-center justify-between ">
-            <Link href={'/'} className="font-bold text-xl">
-                THE BEAR
-            </Link>
+        <div className="flex flex-col items-center pb-2 md:pb-0">
+            <div className="h-12 md:h-16 p-4 lg:px-10 xl:px-20 text-red-500 flex items-center justify-between w-full">
+                <Link href={'/'} className="font-bold text-xl">
+                    THE BEAR
+                </Link>
+                <Link
+                    href="https://github.com/AdrianRMV"
+                    className="hidden md:block text-sm uppercase font-bold text-center text-red-500"
+                >
+                    Adrian Ramirez
+                </Link>
+                <p className="text-sm md:text-base">ALL RIGHTS RESERVED.</p>
+            </div>
             <Link
                 href="https://github.com/AdrianRMV"
-                className="text-sm uppercase font-bold text-center"
+                className="text-sm md:hidden uppercase font-bold text-center text-red-500"
             >
                 Adrian Ramirez
             </Link>
-            <p className="text-sm md:text-base">ALL RIGHTS RESERVED.</p>
         </div>
     );
 };
