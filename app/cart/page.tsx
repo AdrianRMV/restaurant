423ef29bb12011ab4193d5aca6d934e5387ea3c1
AdrianRMV <adrianthelawx@gmail.com>
2023-10-17 18:16:05 -0700

    Stripe Payment

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -2,13 +2,41 @@
 import Image from 'next/image';
 import { useCartStore } from '../utils/store';
 import { useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 const CartPage = () => {
     const { products, totalPrice, totalItems, removeFromCart } = useCartStore();
 
+    const { data: session } = useSession();
+    const router = useRouter();
+
     useEffect(() => {
         useCartStore.persist.rehydrate();
     }, []);
+
+    const handleCheckout = async () => {
+        if (!session) {
+            router.push('/');
+        } else {
+            try {
+                const res = await fetch('http://localhost:3000/api/orders', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({
+                        price: totalPrice,
+                        products,
+                        status: 'Not Paid!',
+                        userEmail: session.user.email,
+                    }),
+                });
+                const data = await res.json();
+                router.push(`/pay/${data.id}`);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    };
 
     return (
         <div className="h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-red-500 lg:flex-row">
@@ -58,7 +86,10 @@ const CartPage = () => {
                     <span className="">Total (IVA) </span>
                     <span className="font-bold">${totalPrice}</span>
                 </div>
-                <button className="bg-red-500 p-3 rounded-md text-white w-1/2 self-end  flex justify-center">
+                <button
+                    className="bg-red-500 p-3 rounded-md text-white w-1/2 self-end  flex justify-center"
+                    onClick={handleCheckout}
+                >
                     CHECKOUT
                 </button>
             </div>
